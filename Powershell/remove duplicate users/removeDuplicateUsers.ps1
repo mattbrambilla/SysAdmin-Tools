b9bd8b2bbf4ad8077e5a05789b8868e1be410893
Mattia Brambilla <m.brambilla@live.it>
2023-08-01 14:42:18 +0200

    feat: added script to remove duplicate users

---
+++ Powershell/remove duplicate users/removeDuplicateUsers.ps1
@@ -0,0 +1,3 @@
+﻿#this script should be used to remove any duplicate email addresses in your windows mail settings 
+
+Remove-Item -Path "folder path located in %localappdata%/packagin/ms aad..." -Force -Confirm:$false 
