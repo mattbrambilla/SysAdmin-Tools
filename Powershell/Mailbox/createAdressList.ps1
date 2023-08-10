a0c688bb8f91329fa4a2aa99d5a6695631978a5f
Mattia Brambilla <m.brambilla@live.it>
2023-08-10 14:27:31 +0200

    feat: added Exchange script to create AL and GAL

---
+++ Powershell/Mailbox/createAdressList.ps1
@@ -0,0 +1,3 @@
+#Create Adress List
+#Fill the field companyName with your company name (without #)
+New-AdressList -Name "companyName" -IncludeRecipients MailboxUsers -ConditionalCompany #companyName
---
+++ Powershell/Mailbox/createGlobalAdressList.ps1
@@ -0,0 +1,3 @@
+#Create GAL
+#Fill the field companyName with your company name (without #)
+New-GlobalAdressList -Name "GALName" -IncludeRecipients AllRecipients -ConditionalCompany #companyName
