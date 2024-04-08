71cda0500bcfeb86a19e7c472a3c2d52ac279d93
mattbrambilla <m.brambilla@live.it>
2024-04-08 11:00:55 +0200

    add eenableRetentionPolicy script to Exchange folder

---
+++ Powershell/Mailbox/enableRetentionPolicy.ps1
@@ -0,0 +1,11 @@
+﻿#This script can help you enable the Retention Policy option for a specific email address in Exchange Online.
+#Feel free to enhance the script and redistribute it in accordance with the GNU Public License.
+
+#First of all connect to exchange online
+Connect-ExchangeOnline
+
+#Select your mailbox and retriew info
+Get-mailbox [Your Mailbox adress here] |fl
+
+#Enable the Retention Policy
+Set-Mailbox [Your Mailbox adress here] -RetentionHoldEnabled $true
