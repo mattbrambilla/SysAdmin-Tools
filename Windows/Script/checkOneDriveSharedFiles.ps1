e03b3ed3932bb7f858af79e59f8eab25f83302a6
mattbrambilla <m.brambilla@live.it>
2024-04-11 16:23:08 +0200

    Add script to check onedrive shared files in tenant

---
+++ Windows/Script/checkOneDriveSharedFiles.ps1
@@ -0,0 +1,29 @@
+﻿# Set your authentication credentials
+$clientId = "TuoClientID"
+$clientSecret = "TuoClientSecret"
+$tenantId = "TuoTenantID"
+
+# Get an access token using your credentials
+$tokenEndpoint = "https://login.microsoftonline.com/$tenantId/oauth2/token"
+$body = @{
+    client_id     = $clientId
+    client_secret = $clientSecret
+    grant_type    = "client_credentials"
+    scope         = "https://graph.microsoft.com/.default"
+}
+$token = Invoke-RestMethod -Uri $tokenEndpoint -Method Post -Body $body
+
+# Set the authorization header
+$authorization = "Bearer $($token.access_token)"
+$headers = @{
+    Authorization = $authorization
+}
+
+# Get the list of files shared via OneDrive
+$sharedFilesEndpoint = "https://graph.microsoft.com/v1.0/drive/shared"
+$sharedFiles = Invoke-RestMethod -Uri $sharedFilesEndpoint -Headers $headers
+
+# View details of shared files
+$sharedFiles.value | ForEach-Object {
+    Write-Output "File condiviso: $($_.name) - URL: $($_.webUrl)"
+}
