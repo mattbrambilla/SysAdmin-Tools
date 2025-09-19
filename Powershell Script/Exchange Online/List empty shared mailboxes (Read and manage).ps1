914552f756417fdafb121e4333c493c23fd99044
Matt <30724389+mattbrambilla@users.noreply.github.com>
2025-09-19 11:42:41 +0200

    Upload script to list empty shared mailboxes
    This is the first version, I want to improve the script in the next months.

---
+++ Powershell Script/Exchange Online/List empty shared mailboxes (Read and manage).ps1
@@ -0,0 +1,6 @@
+﻿#Mi connetto a Exchange
+Connect-ExchangeOnline
+#Cerco tutte le shared mailbox che non hanno nessun utente in Read and manage (Full Access)
+Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited | ? { (Get-MailboxPermission $_.UserPrincipalName | ? {$_.User -ne "NT AUTHORITY\SELF"}).Count -eq 0 }
+#Mantengo aperta la sessione powershell
+Pause
